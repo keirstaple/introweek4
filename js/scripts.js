f6ee4b7f90db73f551183f53608de1bae06d0711
Keir Staple <keirstaple@comcast.net>
2016-01-29 11:08:31 -0800

    create business logic and specs that pass that meet basic assignment requirements

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
-function Receipt (pizzaSize, pizzaToppings, pizzaQuantity) {
+function Receipt (pizzaSize, pizzaToppings) {
   this.pizzaSize = pizzaSize;
   this.pizzaToppings = pizzaToppings;
   this.pizzaPrice = 0;
 }
 
 Receipt.prototype.pizzaSizePrice = function() {
-
-}
+  if (this.pizzaSize === 'Small') {
+    return this.pizzaPrice += 9;
+  } else if (this.pizzaSize === 'Medium') {
+    return this.pizzaPrice += 12;
+  } else if (this.pizzaSize === 'Large') {
+    return this.pizzaPrice += 15;
+  } else {
+    return this.pizzaPrice += 18;
+  }
+};
 
 Receipt.prototype.pizzaToppingsPrice = function() {
-  return
-}
+  for (var i = 0; i < this.pizzaToppings.length; i++) {
+    this.pizzaPrice += 0.50;
+  }
+    return this.pizzaPrice;
+};
 
+//go through list of toppings selected and push them into an array
 
 $(document).ready(function() {
 
--- specs/specs.js
+++ specs/specs.js
@@ -1,8 +1,22 @@
 describe('Receipt', function() {
   it('creates a receipt for the customer based on their order information', function() {
-    var testReceipt = new Receipt ("Large","black olives",0);
+    var testReceipt = new Receipt("Large","black olives",0);
     expect(testReceipt.pizzaSize).to.equal("Large");
     expect(testReceipt.pizzaToppings).to.equal("black olives");
-    expect(testReceipt.pizzaPrice).to.eql(0);
+    expect(testReceipt.pizzaPrice).to.equal(0);
+  });
+});
+
+describe('pizzaSizePrice', function() {
+  it('calculate pizza price based on its size', function() {
+    var testReceipt = new Receipt('Large', 'black olives');
+    expect(testReceipt.pizzaSizePrice()).to.equal(15);
+  });
+});
+
+describe('pizzaToppingsPrice', function() {
+  it('calculate pizza price based on the number of toppings', function() {
+    var testReceipt = new Receipt('Large', ['black olives', 'pepperoni', 'pepperoncini']);
+    expect(testReceipt.pizzaToppingsPrice()).to.equal(1.5);
   });
 });
